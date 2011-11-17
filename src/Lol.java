a911c4a3d0f9f2a4ed9c480301f4ce727bd3657a
Lars Kristian Johansen <larskrjo@gmail.com>
2011-11-17 00:40:18 -0500

    first commit from intellij

---
+++ src/Lol.java
@@ -0,0 +1,16 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: larsen
+ * Date: 11/17/11
+ * Time: 12:38 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Lol {
+
+	public static void main(String[] args){
+
+		System.out.println("yo edouard");
+
+	}
+
+}
