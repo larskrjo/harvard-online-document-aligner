f65b18aedb5c39a2d68c4e6c89ba29221c846033
Edouard Godfrey <edouard.godfrey@gmail.com>
2011-12-04 01:23:02 -0500

    executable jar

---
+++ bin/start3.sh
@@ -0,0 +1,2 @@
+export MPJ_HOME=/home/ljohansen/CS205/mpj/
+java -cp .:$MPJ_HOME/lib/mpj.jar ODA_Cluster 3 mpj.conf niodev
---
+++ bin/start5.sh
@@ -0,0 +1,2 @@
+export MPJ_HOME=/home/ljohansen/CS205/mpj/
+java -cp .:$MPJ_HOME/lib/mpj.jar ODA_Cluster 5 mpj.conf niodev
