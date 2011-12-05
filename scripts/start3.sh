accbb8453fec5f1aedb28bf41b5a688796e42e77
Lars Kristian Johansen <larskrjo@gmail.com>
2011-12-04 21:02:57 -0500

    deleted things, clean jar

--- scripts/start3.sh
+++
@@ -1,2 +0,0 @@
-export MPJ_HOME=/home/ljohansen/CS205/mpj/
-java -cp .:$MPJ_HOME/lib/mpj.jar ODA_Cluster 3 mpj.conf niodev
--- scripts/start_multicore.sh
+++
@@ -1,3 +0,0 @@
-export MPJ_HOME=/home/larsen/mpj/
-export PATH=$PATH:$MPJ_HOME/bin
-mpjrun.sh -np 2 ODA_Multicore >> matches
